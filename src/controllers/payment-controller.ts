cbffead12656d00804c6da516dc8e810f72146b5
erelzohar <zoharerel@gmail.com>
2023-12-06 13:18:17 +0200

    new test

--- src/controllers/payment-controller.ts
+++ src/controllers/payment-controller.ts
@@ -13,8 +13,8 @@ router.use(bodyParser.json({ type: 'application/*+json' }))
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 router.post("/payment", urlencodedParser, async (req, res) => {
     try {
-        console.log(req.body.statusCode);
-        console.log(req.body.customFields.cField1);
+        // console.log(req.body.statusCode);
+        // console.log(req.body.customFields.cField1);
         console.log(req.body);
         const transaction = new Transaction(req.body);
         console.log(transaction);
