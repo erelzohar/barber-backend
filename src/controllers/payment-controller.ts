1c503d1b6ee50c0a56f867d499bfeef665e76bbe
erelzohar <zoharerel@gmail.com>
2023-12-06 14:24:56 +0200

    logs again

--- src/controllers/payment-controller.ts
+++ src/controllers/payment-controller.ts
@@ -17,7 +17,8 @@ router.post("/payment", urlencodedParser, async (req, res) => {
         // console.log(req.body.customFields.cField1);
         console.log(req.body);
         const transaction = new Transaction(req.body);
-        console.log(transaction.payerPhone);
+        transaction.data.orderId = req.body.data.cField1
+        console.log(transaction);
 
         res.sendStatus(200)
     }
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,118 +1,50 @@
 import mongoose, { Document, Schema } from "mongoose";
 
 export interface ITransaction {
-    asmachta:string;
-    cardSuffix: string;
-    cardType: string;
-    cardTypeCode: string;
-    cardBrand: string;
-    cardBrandCode: string;
-    cardExp: string;
-    firstPaymentSum: string;
-    periodicalPaymentSum: string;
     status: string;
-    statusCode: string;
-    transactionTypeId: string;
-    paymentType: string;
-    sum: string;
-    paymentsNum: string;
-    paymentDate: string;
-    description: string;
-    fullName: string;
-    payerPhone: string;
-    allPaymentsNum: string;
-    payerEmail: string;
-    transactionId: string;
-    transactionToken: string;
-    processId: string;
-    processToken: string;
-    orderId: string;
+    data: {
+        asmachta: string;
+        cardSuffix: string;
+        cardType: string;
+        cardTypeCode: string;
+        cardBrand: string;
+        cardBrandCode: string;
+        cardExp: string;
+        firstPaymentSum: string;
+        periodicalPaymentSum: string;
+        statusCode: string;
+        transactionTypeId: string;
+        paymentType: string;
+        sum: string;
+        paymentsNum: string;
+        paymentDate: string;
+        description: string;
+        fullName: string;
+        payerPhone: string;
+        allPaymentsNum: string;
+        payerEmail: string;
+        transactionId: string;
+        transactionToken: string;
+        processId: string;
+        processToken: string;
+        orderId: string;
+    }
+
 }
 
 export interface TransactionModel extends ITransaction, Document<string> { }
 
 const TransactionScheme: Schema = new Schema({
 
-    asmachta: {
-        type: String
-    },
-    cardSuffix: {
-        type: String
-    },
-    cardType: {
-        type: String
-    },
-    cardTypeCode: {
-        type: String
-    },
-    cardBrand: {
-        type: String
-    },
-    cardBrandCode: {
-        type: String
-    },
-    cardExp: {
-        type: String
-    },
-    firstPaymentSum: {
-        type: String
-    },
-    periodicalPaymentSum: {
-        type: String
+    data: {
+        type: Object
     },
     status: {
         type: String
     },
-    statusCode: {
-        type: String
-    },
-    transactionTypeId: {
-        type: String
-    },
-    paymentType: {
-        type: String
-    },
-    sum: {
-        type: String
-    },
-    paymentsNum: {
-        type: String
-    },
-    paymentDate: {
-        type: String
-    },
-    description: {
-        type: String
-    },
-    fullName: {
-        type: String
-    },
-    payerPhone: {
-        type: String
-    },
-    allPaymentsNum: {
-        type: String
-    },
-    payerEmail: {
-        type: String
-    },
-    transactionId: {
-        type: String
-    },
-    transactionToken: {
-        type: String
-    },
-    processId: {
-        type: String
-    },
-    processToken: {
-        type: String
-    },
-    orderId: {
-        type: String
-    },
+
 }, { versionKey: false, toJSON: { virtuals: true }, id: false });
 
 
-export default mongoose.model<TransactionModel>("Transaction", TransactionScheme,'transactions');
+export default mongoose.model<TransactionModel>("Transaction", TransactionScheme, 'transactions');
 
