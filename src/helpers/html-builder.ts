55c1d7939e8caeb53cdffb9ad0be092b710829cd
erelzohar <zoharerel@gmail.com>
2023-12-13 21:06:43 +0200

    still html

--- src/helpers/html-builder.ts
+++ src/helpers/html-builder.ts
@@ -1,6 +1,26 @@
+import path from "path";
 import { OrderModel } from "../models/Order";
+import fs from "fs";
 
 function createOrderHtml(order: OrderModel) {
+    let imagePath = path.join(__dirname, "..", "assets", "images", "mailimage-1.png");
+    let imageBuffer = fs.readFileSync(imagePath);
+    const encodedImage1 = imageBuffer.toString('base64');
+    imagePath = path.join(__dirname, "..", "assets", "images", "mailimage-2.png");
+    imageBuffer = fs.readFileSync(imagePath);
+    const encodedImage2 = imageBuffer.toString('base64');
+    imagePath = path.join(__dirname, "..", "assets", "images", "mailimage-3.png");
+    imageBuffer = fs.readFileSync(imagePath);
+    const encodedImage3 = imageBuffer.toString('base64');
+    imagePath = path.join(__dirname, "..", "assets", "images", "mailimage-4.png");
+    imageBuffer = fs.readFileSync(imagePath);
+    const encodedImage4 = imageBuffer.toString('base64');
+    imagePath = path.join(__dirname, "..", "assets", "images", "mailimage-5.jpeg");
+    imageBuffer = fs.readFileSync(imagePath);
+    const encodedImage5 = imageBuffer.toString('base64');
+    imagePath = path.join(__dirname, "..", "assets", "images", "mailimage-6.png");
+    imageBuffer = fs.readFileSync(imagePath);
+    const encodedImage6 = imageBuffer.toString('base64');
     return `<!DOCTYPE HTML
     PUBLIC "-//W3C//DTD XHTML 1.0 Transitional //EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
   <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml"
@@ -242,7 +262,7 @@ function createOrderHtml(order: OrderModel) {
                                   <tr>
                                     <td style="padding-right: 0px;padding-left: 0px;" align="center">
                                       <a href="https://unlayer.com" target="_blank">
-                                        <img align="center" border="0" src="assets/images/mailimage-6.png" alt="Logo" title="Logo"
+                                        <img align="center" border="0" src="data:image/*;base64,${encodedImage6}" alt="Logo" title="Logo"
                                           style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 16%;max-width: 92.8px;"
                                           width="92.8" class="v-src-width v-src-max-width" />
                                       </a>
@@ -369,7 +389,7 @@ function createOrderHtml(order: OrderModel) {
                                   <tr>
                                     <td style="padding-right: 0px;padding-left: 0px;" align="center">
                                       <a href="https://www.donaroma-il.com" target="_blank">
-                                        <img align="center" border="0" src="assets/images/mailimage-5.jpeg" alt="" title=""
+                                        <img align="center" border="0" src="data:image/*;base64,${encodedImage5}" alt="" title=""
                                           style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 100%;max-width: 600px;"
                                           width="600" class="v-src-width v-src-max-width" />
                                       </a>
@@ -731,7 +751,7 @@ function createOrderHtml(order: OrderModel) {
                                   style="font-size: 14px; color: #333333; line-height: 160%; text-align: right; word-wrap: break-word;">
                                   <p style="line-height: 160%;">
                                     <span style="font-size: 18px; line-height: 28.8px;">
-                                    ${order.street + " " + order.streetNum + " " + order.city + " " + (order.aptNum ? "דירה "+ order.aptNum : '')}
+                                    ${order.street + " " + order.streetNum + " " + order.city + " " + (order.aptNum ? "דירה " + order.aptNum : '')}
                                   </span>
                                   </p>
                                 </div>
@@ -751,7 +771,7 @@ function createOrderHtml(order: OrderModel) {
   
                                 <div
                                   style="font-size: 14px; color: #333333; line-height: 160%; text-align: right; word-wrap: break-word;">
-                                  <p style="line-height: 160%;"><span style="font-size: 18px; line-height: 28.8px;">סוג
+                                  <p style="line-height: 160%;"><span style="font-size: 18px; line-height: 28.8px;">
                                     ${order.deliveryType === "express" ? "משלוח אקספרס עד 3 ימי עסקים" : order.deliveryType === "regular" ? "משלוח רגיל עד 8 ימי עסקים" : "איסוף עצמי"}
                                     </span>
                                   </p>
@@ -880,7 +900,7 @@ function createOrderHtml(order: OrderModel) {
                                   <tr>
                                     <td style="padding-right: 0px;padding-left: 0px;" align="center">
                                       <a href="Help Line Icon" target="_blank">
-                                        <img align="center" border="0" src="assets/images/mailimage-4.png" alt="Icon" title="Icon"
+                                        <img align="center" border="0" src="data:image/*;base64,${encodedImage4}" alt="Icon" title="Icon"
                                           style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: inline-block !important;border: none;height: auto;float: none;width: 15%;max-width: 87px;"
                                           width="87" class="v-src-width v-src-max-width" />
                                       </a>
@@ -1013,7 +1033,7 @@ function createOrderHtml(order: OrderModel) {
                                             style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
                                             <a href="https://www.instagram.com/don_aromaisr/" title="Instagram"
                                               target="_blank">
-                                              <img src="assets/images/mailimage-2.png" alt="Instagram" title="Instagram" width="32"
+                                              <img src="data:image/*;base64,${encodedImage2}" alt="Instagram" title="Instagram" width="32"
                                                 style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
                                             </a>
                                           </td>
@@ -1031,7 +1051,7 @@ function createOrderHtml(order: OrderModel) {
                                             style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
                                             <a href="https://www.facebook.com/profile.php?id=61550822289202&mibextid=LQQJ4d"
                                               title="Facebook" target="_blank">
-                                              <img src="assets/images/mailimage-1.png" alt="Facebook" title="Facebook" width="32"
+                                              <img src="data:image/*;base64,${encodedImage1}" alt="Facebook" title="Facebook" width="32"
                                                 style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
                                             </a>
                                           </td>
@@ -1048,7 +1068,7 @@ function createOrderHtml(order: OrderModel) {
                                           <td align="left" valign="middle"
                                             style="word-break: break-word;border-collapse: collapse !important;vertical-align: top">
                                             <a href="https://wa.me/972503713852" title="WhatsApp" target="_blank">
-                                              <img src="assets/images/mailimage-3.png" alt="WhatsApp" title="WhatsApp" width="32"
+                                              <img src="data:image/*;base64,${encodedImage3}" alt="WhatsApp" title="WhatsApp" width="32"
                                                 style="outline: none;text-decoration: none;-ms-interpolation-mode: bicubic;clear: both;display: block !important;border: none;height: auto;float: none;max-width: 32px !important">
                                             </a>
                                           </td>
@@ -1142,6 +1162,6 @@ function createOrderHtml(order: OrderModel) {
   </html>`
 }
 
-export default{
+export default {
     createOrderHtml
 }
