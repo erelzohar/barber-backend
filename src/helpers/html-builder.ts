a4005803c0dd452cc8d4d907a78152c3f6393830
erelzohar <zoharerel@gmail.com>
2023-12-17 18:57:54 +0200

    fixed html column

--- src/helpers/html-builder.ts
+++ src/helpers/html-builder.ts
@@ -184,12 +184,12 @@ function createOrderHtml(order: OrderModel) {
             <div class="u-row-container" style="padding: 0px;background-color: transparent">
               <div class="u-row"
                 style="margin: 0 auto;min-width: 320px;max-width: 600px;overflow-wrap: break-word;word-wrap: break-word;word-break: break-word;background-color: transparent;">
-                <div
+                <div 
                   style="border-collapse: collapse;display: table;width: 100%;height: 100%;background-color: transparent;">
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: transparent;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="600" style="width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-100"
+                  <div id='1try' class="u-col u-col-100"
                     style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
                     <div
                       style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
@@ -245,7 +245,7 @@ function createOrderHtml(order: OrderModel) {
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: #ffffff;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="600" style="width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-100"
+                  <div id='3try' class="u-col u-col-100"
                     style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
                     <div
                       style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
@@ -301,7 +301,7 @@ function createOrderHtml(order: OrderModel) {
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: #ffffff;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="600" style="width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-100"
+                  <div id='4try' class="u-col u-col-100"
                     style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
                     <div
                       style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
@@ -372,7 +372,7 @@ function createOrderHtml(order: OrderModel) {
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: transparent;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="600" style="width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-100"
+                  <div id='5try' class="u-col u-col-100"
                     style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
                     <div
                       style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
@@ -428,7 +428,7 @@ function createOrderHtml(order: OrderModel) {
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: #dde5e7;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="600" style="width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-100"
+                  <div id='6try' class="u-col u-col-100"
                     style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
                     <div
                       style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
@@ -484,45 +484,6 @@ function createOrderHtml(order: OrderModel) {
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: #ffffff;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="300" style="width: 300px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-50"
-                    style="max-width: 320px;min-width: 300px;display: table-cell;vertical-align: top;">
-                    <div
-                      style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
-                      <!--[if (!mso)&(!IE)]><!-->
-                      <div
-                        style="box-sizing: border-box; height: 100%; padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
-                        <!--<![endif]-->
-  
-  
-                        <table style="font-family:arial,helvetica,sans-serif;" role="presentation" cellpadding="0"
-                          cellspacing="0" width="100%" border="0">
-                          <tbody>
-                            <tr>
-                              <td class="v-container-padding-padding"
-                                style="overflow-wrap:break-word;word-break:break-word;padding:10px;font-family:arial,helvetica,sans-serif;"
-                                align="left">
-  
-                                <table height="0px" align="center" border="0" cellpadding="0" cellspacing="0" width="76%"
-                                  style="border-collapse: collapse;table-layout: fixed;border-spacing: 0;mso-table-lspace: 0pt;mso-table-rspace: 0pt;vertical-align: top;border-top: 1px solid #BBBBBB;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%">
-                                  <tbody>
-                                    <tr style="vertical-align: top">
-                                      <td
-                                        style="word-break: break-word;border-collapse: collapse !important;vertical-align: top;font-size: 0px;line-height: 0px;mso-line-height-rule: exactly;-ms-text-size-adjust: 100%;-webkit-text-size-adjust: 100%">
-                                        <span>&#160;</span>
-                                      </td>
-                                    </tr>
-                                  </tbody>
-                                </table>
-  
-                              </td>
-                            </tr>
-                          </tbody>
-                        </table>
-  
-                        <!--[if (!mso)&(!IE)]><!-->
-                      </div><!--<![endif]-->
-                    </div>
-                  </div>
                   <!--[if (mso)|(IE)]></td><![endif]-->
                   <!--[if (mso)|(IE)]><td align="center" width="300" style="width: 300px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
                   <div class="u-col u-col-50"
@@ -620,7 +581,7 @@ function createOrderHtml(order: OrderModel) {
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: #dde5e7;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="600" style="width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-100"
+                  <div id='8try' class="u-col u-col-100"
                     style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
                     <div
                       style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
@@ -676,7 +637,7 @@ function createOrderHtml(order: OrderModel) {
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: #ffffff;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="600" style="width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-100"
+                  <div id='9try' class="u-col u-col-100"
                     style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
                     <div
                       style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
@@ -788,7 +749,7 @@ function createOrderHtml(order: OrderModel) {
                   <!--[if (mso)|(IE)]><table width="100%" cellpadding="0" cellspacing="0" border="0"><tr><td style="padding: 0px;background-color: transparent;" align="center"><table cellpadding="0" cellspacing="0" border="0" style="width:600px;"><tr style="background-color: #dde5e7;"><![endif]-->
   
                   <!--[if (mso)|(IE)]><td align="center" width="600" style="width: 600px;padding: 0px;border-top: 0px solid transparent;border-left: 0px solid transparent;border-right: 0px solid transparent;border-bottom: 0px solid transparent;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;" valign="top"><![endif]-->
-                  <div class="u-col u-col-100"
+                  <div id='10try' class="u-col u-col-100"
                     style="max-width: 320px;min-width: 600px;display: table-cell;vertical-align: top;">
                     <div
                       style="height: 100%;width: 100% !important;border-radius: 0px;-webkit-border-radius: 0px; -moz-border-radius: 0px;">
