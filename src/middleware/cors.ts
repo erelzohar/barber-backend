c973dbebbc6a30e1ddb1aa21eabb9ac1230b5711
erelzohar <zoharerel@gmail.com>
2023-12-28 11:25:06 +0200

    log origin

--- src/middleware/cors.ts
+++ src/middleware/cors.ts
@@ -4,6 +4,8 @@ import { Request, Response, NextFunction } from "express";
 export default function useCors(req: Request, res: Response, next: NextFunction) {
     const allowedOrigins = ["https://www.donaroma-il.com", "https://meshulam.co.il"];
     if (process.env.NODE_ENV === "production") {
+        console.log(req.headers.origin);
+
         if (allowedOrigins.includes(req.headers.origin)) res.header("Access-Control-Allow-Origin", req.headers.origin);
         else return res.status(403).json('Not allowed');
     }
