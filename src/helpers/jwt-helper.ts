7d7a8715498a52c6ab15c9a2656ebb5492c0934e
erelzohar <zoharerel@gmail.com>
2024-06-19 18:20:58 +0300

    cancel login exp

--- src/helpers/jwt-helper.ts
+++ src/helpers/jwt-helper.ts
@@ -3,5 +3,5 @@ import jwt from "jsonwebtoken";
 import { AdminModel } from "../models/Admin";
 
 export default function getNewToken(payload:AdminModel) { // (payload will be the user object)
-    return jwt.sign({ payload }, config.server.jwtKey,{expiresIn:"30d"});
+    return jwt.sign({ payload }, config.server.jwtKey);
 }
